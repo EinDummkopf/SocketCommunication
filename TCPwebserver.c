cf21e54b80d218c1285ff3e8edbc6a2dff18d416
EinDummkopf <songtaehwan@icloud.com>
2024-01-15 22:39:36 +0900

    bind() error handling

--- TCPwebserver.c
+++ TCPwebserver.c
@@ -32,6 +32,15 @@ int main(int argc, char **argv){
     if(serv_sock == -1){
         error_handling("socket() error");
     }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    serv_addr.sin_port=htonl(atoi(argv[1]));
+
+    if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1){
+        error_handling("bind() error");
+    }
 }
 
 void error_handling(char *message){
